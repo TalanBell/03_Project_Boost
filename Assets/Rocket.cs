f4519c6e0b78cb53935e920c3a22d1c226e53592
TalanBell <harpocrat@hotmail.com>
2020-12-26 16:08:35 +0000

    Updated to Unity 2020.1.14f1

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -17,16 +17,6 @@ public class Rocket : MonoBehaviour
         rocketSound = GetComponent<AudioSource>();
     }
 
-    //void OnCollisionEnter(Collision collision)
-    //{
-    //    foreach (ContactPoint contact in collision.contacts)
-    //    {
-    //        Debug.DrawRay(contact.point, contact.normal, Color.white);
-    //    }
-    //    if (collision.relativeVelocity.magnitude > 2)
-    //        audioSource.Play();
-    //}
-
     // Update is called once per frame
     void Update () {
         Thrust();
@@ -89,6 +79,5 @@ public class Rocket : MonoBehaviour
             | RigidbodyConstraints.FreezeRotationY
             | RigidbodyConstraints.FreezePositionZ;
     }
-
 
 }
